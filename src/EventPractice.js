78124465e56c42edd90d254df331667d2d8eb41e
강영모 <zrookie@naver.com>
2021-01-22 11:18:38 +0900

    2021.01.22

---
+++ src/EventPractice.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class EventPractice extends Component {
+  state = {
+    message: '',
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력해 보세요."
+          value={this.state.message}
+          onChange={(e) => {
+            // console.log(e);
+            // console.log(e.target.value);
+            this.setState({
+              message: e.target.value,
+            });
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default EventPractice;
